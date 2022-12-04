b61292af777d3d5e5164407daf6c10a26db51530
Ian Weller <maladroitthief@gmail.com>
2022-12-04 15:26:23 -0500

    feat: Camp Cleanup
    - Day 4 is rekt yo

--- cmd/2022/main.go
+++ cmd/2022/main.go
@@ -4,10 +4,12 @@ import (
 	"github.com/maladroitthief/advent-of-code/pkg/2022/day01"
 	"github.com/maladroitthief/advent-of-code/pkg/2022/day02"
 	"github.com/maladroitthief/advent-of-code/pkg/2022/day03"
+	"github.com/maladroitthief/advent-of-code/pkg/2022/day04"
 )
 
 func main() {
 	day01.Puzzle()
 	day02.Puzzle()
 	day03.Puzzle()
+	day04.Puzzle()
 }
